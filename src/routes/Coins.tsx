cb8e81fd90b9c8514251d957cc3053fe4f4e2c49
rigood <rigood@naver.com>
2022-09-07 00:39:57 +0900

    Coins.tsx
    1. styled 컴포넌트 생성
    2 useState 이용하여 coins, loading 상태관리, CoinInterface 설정
    3. useEffect, async/await 이용하여 Coin data-fetching
    4. 컴포넌트 배치 및 map()을 이용한 Coin 렌더링
    5. Link 이용하여 Coin 상세페이지로 이동
    7. Coin image-fetching

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,7 +1,63 @@
-function Coins() {
-  return (
-    <div>Coins</div>
-  )
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Container = styled.div``;
+
+const Header = styled.header``;
+
+const Title = styled.h1``;
+
+const CoinList = styled.ul``;
+
+const Loader = styled.span``;
+
+const Coin = styled.li``;
+
+const Img = styled.img``;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
 }
 
-export default Coins
+function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      {loading ? (
+        <Loader>로딩중</Loader>
+      ) : (
+        <CoinList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`/${coin.id}`}>
+                <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinList>
+      )}
+    </Container>
+  );
+}
+
+export default Coins;
