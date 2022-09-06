70b33747228fcec3abf9eb8cd01edf11bc1edda4
rigood <rigood@naver.com>
2022-09-07 00:59:21 +0900

    Coin.tsx
    1. styled 컴포넌트 생성
    2. useState 이용하여 loading 상태관리
    3. useLocation 이용하여 Coins.tsx에서 Link를 통해 보내준 coin.name 받아옴,  LocationState 인터페이스 설정
    4. 컴포넌트 배치

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,10 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import { useState } from "react";
 
-function Coin() {
-    const coinId = useParams();
-  return (
-    <div>Coin</div>
-  )
+const Container = styled.div``;
+
+const Header = styled.header``;
+
+const Title = styled.h1``;
+
+const Loader = styled.span``;
+
+interface LocationState {
+  state: {
+    name: string;
+  };
 }
 
-export default Coin
+function Coin() {
+  const coinId = useParams();
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation() as LocationState;
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "로딩중"}</Title>
+      </Header>
+      {loading ? <Loader>로딩중</Loader> : null}
+    </Container>
+  );
+}
+
+export default Coin;
