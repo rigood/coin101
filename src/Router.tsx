130b75f03edb768dce2d25a72b394246b83e2f34
rigood <rigood@naver.com>
2022-09-09 23:23:24 +0900

    Change Router to HashRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Coins />}></Route>
         <Route path=":coinId/*" element={<Coin />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
